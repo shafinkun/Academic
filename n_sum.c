8d59f66537c9e36ebafbba0d22af2ad7f349d223
Hasnat Shafin <sh.shafin20@gmail.com>
2023-02-28 12:48:28 +0600

    changed while to do while

--- n_sum.c
+++ n_sum.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n=100,sum=0,i=1;
-    while(i<=n)
+    int n=10,sum=0,i=0;
+    do
     {
         sum=sum+i;
         i++;
-    }
+    }while(i<=n);
+
     printf("Sum= %d",sum);
 }
