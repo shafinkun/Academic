0d26be1f37c96fb857123e0b740d407ef3bc53b9
Hasnat Shafin <sh.shafin20@gmail.com>
2023-02-28 21:03:53 +0600

    beecrowd 1004

--- printnthnumbersafterdecimal.cpp/print.cpp
+++ printnthnumbersafterdecimal.cpp/print.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main()
 {
-    float n;
+    float n,j;
     cout<<"Give any float number: ";
     cin>>n;
-    cout<<fixed<<setprecision(12);
+    cout<<"Give n for decimal to print: ";
+    cin>>j;
+    cout<<fixed<<setprecision(j);
+    cout<<"The number is: "<<n<<endl;
     return 0;
 }
