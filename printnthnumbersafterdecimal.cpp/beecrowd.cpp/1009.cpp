5f55a3a31a325a04581989713a8d726d18969607
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-01 16:23:01 +0600

    beecrowd 1009

--- printnthnumbersafterdecimal.cpp/beecrowd.cpp/1009.cpp
+++ printnthnumbersafterdecimal.cpp/beecrowd.cpp/1009.cpp
@@ -1,14 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include<iomanip>
-        using namespace std;
-        int main()
-       {
-        char M[100];
-        cin>>M;
-        double a,b,c;
-        cin>>a>>b;
-        c=a+(b*15)/100;
-        cout<<fixed<<setprecision(2);
-        cout<<"TOTAL = R$ "<<c<<endl;
-        return 0;
-    }          
+using namespace std;
+ 
+int main() {
+ 
+    char name[50];
+    double a,b;
+    cout<<"What's your name? ";
+    cin>>name;
+    cout<<"Monthly salary: ";
+    cin>>a;
+    cout<<"Total sale: ";
+    cin>>b;
+    float c=(a+(b*.15));
+    cout<<fixed;
+    cout<<setprecision(2)<<"TOTAL = R$ "<<c<<endl;
+    return 0;
+}          
