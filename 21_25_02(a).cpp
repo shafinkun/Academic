46a52151273841817254730640a93f28bef00c7f
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-04-04 22:58:44 +0600

    Assignment Done

--- 21_25_02(a).cpp
+++ 21_25_02(a).cpp
@@ -1,5 +1,6 @@
-// Linked list traversing
+/*______________________Implementation of Linked List_______________________*/
 /*__________________Solved by Shahriar Hasnat Shafin Ahmed__________________*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,25 +13,75 @@ void traverse(int START, vector<int> &LINK, vector<string> &INFO)
         cout << INFO[PTR] << " ";
         PTR = LINK[PTR];
     }
+    cout << endl;
 }
 
-void insert(string item, int pos, int START, vector<int> &LINK, vector<string> &INFO)
+void insert(int &START, int POS, vector<int> &LINK, vector<string> &INFO, string ITEM)
 {
-    INFO.push_back(item);
+    int PTR, COUNT, AVAIL, NEW;
+    AVAIL = INFO.size();
+    NEW = AVAIL;
+    INFO.push_back(ITEM);
+    LINK.push_back(0);
+
+    if (POS == 1)
+    {
+
+        LINK[NEW] = START;
+        START = NEW;
+    }
+    else
+    {
+        PTR = START;
+        COUNT = 1;
+
+        while (COUNT < POS - 1)
+        {
+            PTR = LINK[PTR];
+            COUNT++;
+        }
+
+        LINK[NEW] = LINK[PTR];
+        LINK[PTR] = NEW;
+    }
+}
+
+void Delete(int &START, int POS, vector<int> &LINK, vector<string> &INFO)
+{
+    int PTR, COUNT;
+    PTR = START;
+    COUNT = 1;
+    if (POS == 1)
+    {
+        START = LINK[START];
+    }
+    else
+    {
+        while (COUNT < POS - 1)
+        {
+            PTR = LINK[PTR];
+            COUNT++;
+        }
+        LINK[PTR] = LINK[LINK[PTR]];
+    }
 }
 
 int main()
 {
-    int START = 2, PTR;
+    int START = 2;
 
-    //used vector to store the strings because it is dynamic
-    vector<int> LINK = {4, -1, 3, 0, 1};
-    vector<string> INFO = {"you", "shafin", "I", "love", ""};
-    
+    // I used vector to store the linked list because it is dynamic
+    vector<int> LINK = {1, -1, 3, 0};
+    vector<string> INFO = {"you", "shafin!", "I", "hate"};
+
     traverse(START, LINK, INFO);
 
+    // Let's delete "hate" and put "love" in the 2nd position
+    Delete(START, 2, LINK, INFO);
+    traverse(START, LINK, INFO);
 
-    
+    insert(START, 2, LINK, INFO, "love");
+    traverse(START, LINK, INFO);
 
     return 0;
 }
--- 21_25_02(b).cpp
+++ 21_25_02(b).cpp
@@ -1,57 +1,161 @@
-// Linked list insertion
-/*__________________Solved by Shahriar Hasnat Shafin Ahmed__________________*/
+// Implement an List and implement the following functions:
+// 1. Insert an element
+// 2. Delete an element
+// 3. Traverse the list
+// 4. Bubble sort the list
+// 5. Marge two lists
+// 6. Search an element
+
+/*____________________Solved by Shahriar Hasnat Shafin Ahmed___________________*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void insert(int pos, string new_data, int START, int LINK[], string INFO[], int size)
+#define MAX_SIZE 100
+
+int insert(int arr[], int &n, int val)
 {
-    // Check if the array is full
-    if (size == 5)
+    if (n >= MAX_SIZE)
     {
-        cout << "Overflow";
-        return;
+        return -1;
     }
-
-    // Shift elements to the right of the position
-    for (int i = size; i > pos; i--)
-    {
-        INFO[i] = INFO[i - 1];
-        LINK[i] = LINK[i - 1];
-    }
-
-    // Insert new element and update link
-    INFO[pos] = new_data;
-    LINK[pos] = pos + 1;
-
-    // If inserting at the end, update the link to -1
-    if (pos == size)
-    {
-        LINK[pos] = -1;
-    }
-
-    // If inserting at the start, update START
-    if (pos == 0)
-    {
-        START = 0;
-    }
-
-    // Increment size
-    size++;
+    arr[n] = val;
+    n++;
+    return 0;
 }
 
+void deleteElement(int arr[], int &n, int val)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == val)
+        {
+            break;
+        }
+    }
+    if (i < n)
+    {
+        n = n - 1;
+        for (int j = i; j < n; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
+    }
+}
+
+void traverse(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void mergeLists(int arr1[], int n1, int arr2[], int n2, int merged[])
+{
+    for (int i = 0; i < n1; i++)
+    {
+        merged[i] = arr1[i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        merged[i + n1] = arr2[i];
+    }
+}
+
+int search(int arr[], int n, int val)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == val)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void display(int a[], int sz)
+{
+    cout << "Array is: ";
+    for(int i = 0; i < sz; ++i)
+    {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+}
 int main()
 {
-    int START = 2, PTR, LINK[5] = {4, -1, 3, 0, 1};
-    string INFO[6] = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
-    int size = 5;
+    int arr1[MAX_SIZE] = {5, 3, 2, 4, 1};
+    int arr2[MAX_SIZE] = {10, 8, 7, 9, 6, 11};
+    int merged[MAX_SIZE * 2];
+    int n1 = 5;
+    int n2 = 6;
 
-    insert(2, "Mango", START, LINK, INFO, size);
-    PTR = START;
-    while (PTR != -1)
+
+    // // Traverse the list
+    cout << "Array no. 1: ";
+    traverse(arr1, n1); // works same as display
+
+
+    // Insert an element
+    cout << "After inserting 0 ";
+    insert(arr1, n1, 0); 
+    display(arr1, n1);
+
+
+    // // Delete an element
+    cout << "After deleting 0 ";
+    deleteElement(arr1, n1, 0); 
+    display(arr1, n1); //as in the deleteElement() n1 is given as reference so n1 will automatically updated
+
+
+
+    // // Merge two lists
+    cout << "After merging ";
+    mergeLists(arr1, n1, arr2, n2, merged); // merged becomes {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    display(merged, n1 + n2);
+
+
+    // // Search an element
+    int look = 0;
+    int index = search(merged, n1 + n2, look);
+    cout << look << " is ";
+    
+    if(index != -1)
     {
-        cout << INFO[PTR] << ", ";
-        PTR = LINK[PTR];
+        cout << "at " << index << endl;
     }
+    else
+    {
+        cout << "not available" << endl;
+    }
+    
+
+    // // Bubble sort the list
+    cout << "After soring ";
+    bubbleSort(merged, n1 + n2);
+    display(merged, n1 + n2);
+
+    
 
     return 0;
 }
