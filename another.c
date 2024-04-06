7d749071bb185c27a1ce4213ffa9b920ddda340e
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-04-06 23:59:34 +0600

    Updated Code

--- another.c
+++ another.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{   //this is a test
+{ // this is a test
     int i;
-    for(i=1,i<=1000,i++)
-        {
-    printf("\n\t %d",i);}
+    for (i = 1; i <= 1000; i++)
+    {
+        printf("\n\t %d", i);
+    }
     return 0;
     /*this is high time you learnt programing language by this
     free time. you can't get it in life ever.*/
