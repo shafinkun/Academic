c479fc025ad4cccee6f0a224bcdfbc45eca8d9c2
shafinkun <sh.shafin20@gmail.com>
2024-04-21 02:11:28 +0600

    Java Lab

---
+++ JAVA/Palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/* Problem statement: Write a Java code to check if a user given string is palindrome
+ * 
+ * Solved by : Shahriar Hasnat Shafin Ahmed
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
